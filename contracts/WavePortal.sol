4555f3450b6047acc583088bfc3030c2e2e93759
r2williams <rebecca.williams@historicengland.org.uk>
2022-02-06 16:40:38 +0000

    Created message and get total messages functions

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalMessages;
+
     constructor() {
         console.log("I am a smart contract");
+    }
+
+    function message() public {
+        totalMessages += 1;
+
+        // msg.sender is wallet address of person who called the function
+        console.log("% has sent a message!", msg.sender);
+    }
+
+    function getTotalMessages() public view returns (uint256) {
+        console.log("We have %d total messages!", totalMessages);
+        return totalMessages;
     }
 }
